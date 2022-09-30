01c31613fb119007d7c35475846d9fe19b0bec42
Daniel <datallah@umich.edu>
2022-09-30 04:00:36 +0000

    config changes

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
-# create global variables
+### create global variables ###
+# login information
 email = 'datallah@umich.edu'
 pswrd = 'sabsihdan'
+client_id = 'ab8fa8e7dae440419150fc06881fb815'
+client_secret = '2e2fb26f3ab34891966d689bb2994725'
+scope = "user-library-read"
+min_yr = 1971
+max_yr = 2021
+wiki_url_prefix = 'https://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_'
