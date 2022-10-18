0d2215f0c6df1e7f97c6bb7a1e4aacb20a9bb8a7
Daniel <datallah@umich.edu>
2022-10-18 01:49:10 +0000

    overwriting repo with deepnote files

---
+++ src/config.py
@@ -0,0 +1,10 @@
+### create global variables ###
+# login information
+email = 'datallah@umich.edu'
+pswrd = 'sabsihdan'
+client_id = '1d752d7e921a4d6ea1944686f63cb80b'
+client_secret = '3a7a21006bbe4fd1b24bdc50e7851065'
+scope = "user-library-read"
+min_yr = 1971
+max_yr = 2021
+wiki_url_prefix = 'https://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_'
